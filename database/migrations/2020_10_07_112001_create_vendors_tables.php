59395c4aaf871b96b5b225b82da7a59e40d00ea4
nikolayandreev <contact@nandreev.net>
2020-10-27 18:29:53 +0200

    Progress

--- database/migrations/2020_10_07_112001_create_vendors_tables.php
+++ database/migrations/2020_10_07_112001_create_vendors_tables.php
@@ -23,6 +23,52 @@ class CreateVendorsTables extends Migration
                   ->references('id')->on('users')
                   ->onDelete('cascade')->onUpdate('cascade');
         });
+
+        Schema::create('areas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('vendor_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('vendor_id')->unsigned();
+            $table->bigInteger('area_id')->unsigned();
+            $table->string('street');
+            $table->string('additional')->nullable();
+
+            $table->foreign('area_id')
+                  ->references('id')->on('areas')
+                  ->onDelete('restrict')->onUpdate('cascade');
+
+            $table->foreign('vendor_id')
+                  ->references('id')->on('vendors')
+                  ->onDelete('restrict')->onUpdate('cascade');
+        });
+
+        Schema::create('vendor_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('vendor_id')->unsigned();
+            $table->smallInteger('day_of_week');
+            $table->time('opens_at', '');
+            $table->time('closes_at');
+
+            $table->foreign('vendor_id')
+                  ->references('id')->on('vendors')
+                  ->onDelete('cascade')->onUpdate('cascade');
+
+            $table->unique(['vendor_id', 'day_of_week']);
+        });
+
+        Schema::create('vendor_holidays', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('vendor_id')->unsigned();
+            $table->date('date_from');
+            $table->date('date_to');
+
+            $table->foreign('vendor_id')
+                  ->references('id')->on('vendors')
+                  ->onDelete('cascade')->onUpdate('cascade');
+        });
     }
 
     /**
@@ -32,6 +78,10 @@ class CreateVendorsTables extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('addresses');
+        Schema::dropIfExists('vendor_schedules');
+        Schema::dropIfExists('vendor_holidays');
         Schema::dropIfExists('vendors');
+        Schema::dropIfExists('areas');
     }
 }
